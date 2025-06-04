434774ce29d6e0601f875cc8ac8ae3010bbafb8c
kellynshuti9 <k.dushimima@alustudent.com>
2025-06-04 21:41:32 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
 """This is the state class"""
-from sqlalchemy.ext.declarative import declarative_base
+
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 import models
-from models.city import City
 import shlex
 
 
 class State(BaseModel, Base):
     """This is the class for State
     Attributes:
-        name: input name
+        name (str): input name of the state
+        cities (relationship): SQLAlchemy relationship to City objects
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+
+    # SQLAlchemy relationship: this will be used with DBStorage
+    cities = relationship(
+        "City",
+        cascade='all, delete, delete-orphan',
+        backref="state"
+    )
 
     @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    def cities_list(self):
+        """Returns the list of City instances related to this State
+        when using FileStorage
+        """
+        all_objs = models.storage.all()
+        return [
+            obj for obj in all_objs.values()
+            if obj.__class__.__name__ == 'City' and obj.state_id == self.id
+        ]
